76632590756b9d85a14bd1f35c01c4a1e4373533
hegmat13 <43390549+hegmat13@users.noreply.github.com>
2019-04-26 16:49:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) { //Start of the server
         }
         }
         
-        let fileName = files[j] + "/date.txt";
+        let fileName = "public/Raves/" files[j] + "/date.txt";
         let EntireFile = readFile(fileName); 
 
         let lines = EntireFile.split("\n");  
